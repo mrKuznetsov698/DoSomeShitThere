c959179f43b94ad301f52fc7d9fcf74ccdba6e00
Arsenii <86411768+mrKuznetsov698@users.noreply.github.com>
2023-11-10 16:07:00 +0000

    dfjdgfuidgf

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 let canvas;
 let ctx;
+let angl_rng;
+
+const WIDTH = 600;
+const HEIGHT = 700;
+
+function drawSquare(x, y, radius) {
+    ctx.beginPath()
+    ctx.ellipse(x, y, radius, radius, 0, 0, 2 * Math.PI)
+    ctx.stroke();
+  }
+
+let angl = 0;
 
 function setup() {
-    canvas = document.getElementById('canvas');
+    canvas = document.getElementById('myCanvas');
     ctx = canvas.getContext('2d');
-    
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = 'white';
+    angl_rng = document.getElementById('angle');
+    angl_rng.oninput = () => {
+        // drawCircle(WIDTH / 2, HEIGHT / 2 - 180 + Number(angl_rng.value), 50)
+        angl = Number(angl_rng.value) / 180 * Math.PI;
+        // ctx.arc(, 30, 0, 2 * Math.PI);
+        console.log(angl_rng.value);
+    }
 }
